88d0fe05a9439b34542fc797f48c2cd550e6edb4
Extra <extrahash@protonmail.com>
2019-11-23 22:28:10 -0700

    remove whitespace only lines

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,33 @@
 import System.IO(readFile, writeFile)
 import System.Environment(getArgs)
-import Data.Char (toUpper)
 import Data.List (group)
 
 main :: IO ()
 main = do
     fileName <- head <$> getArgs
-    contents <- readFile fileName
 
-    let result = formatFile contents
+    contents <- lines <$> readFile fileName
+    let removedWhitespaces = map trimWhitespaces contents
+    let result = unlines removedWhitespaces
 
-
-
+    let cleanResult = trimNewlines result
     let outputFileName = createOutputFileName fileName
-    writeFile outputFileName result
 
+    writeFile outputFileName cleanResult
+    
 createOutputFileName :: String -> String
 createOutputFileName fileName = fileName ++ ".out.hs"
 
-formatFile :: String -> String
-formatFile contents = concatMap removeExcessNewlines (group contents)
+trimNewlines :: String -> String
+trimNewlines contents = concatMap enforceTwoSequentialNewlineMaximum (group contents)
 
-removeExcessNewlines :: String -> String
-removeExcessNewlines (x:xs) = if x == '\n' && length xs > 2 then "\n\n" else x:xs
+enforceTwoSequentialNewlineMaximum :: String -> String
+enforceTwoSequentialNewlineMaximum (x:xs) = if x == '\n' && length xs > 2 then "\n\n" else x:xs
+
+trimWhitespaces :: String -> String
+trimWhitespaces line = if hasOnlyWhitespace line 
+    then ""
+    else line
+
+hasOnlyWhitespace :: String -> Bool
+hasOnlyWhitespace = all (`elem` ['\t', ' '])
