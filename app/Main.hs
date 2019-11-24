f77313e40f2085cf95a71eb73ec69a93846050cb
Extra <extrahash@protonmail.com>
2019-11-24 00:38:16 -0700

    make sure there's always exactly one newline at the end o the file

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,27 @@ import Data.List (group)
 main :: IO ()
 main = do
     fileName <- head <$> getArgs
-    writeFile (fileName ++ "-out.hs") 
+    writeFile (fileName ++ ".out.hs") 
         =<<  stringOps
         . unlines 
         . lineOps 
         . lines 
         <$> readFile fileName
         where
-            lineOps = map trimLastWhitespaces . filter (not . isWhitespace)
+            lineOps =  trimLastNewlines .map trimLastWhitespaces . filter (not . isWhitespace)
             stringOps = trimNewlines 
 
-isWhitespace :: String -> Bool
+isWhitespace :: String -> Bool     
 isWhitespace [] = False
-isWhitespace line = all (`elem` [' ', '\t']) line
+isWhitespace line = all (`elem` [' ', '\t']) line       
 
 trimNewlines :: String -> String
 trimNewlines = 
     concatMap enforceTwoSequentialNewlineMax . group  
 
-enforceTwoSequentialNewlineMax :: String -> String
-enforceTwoSequentialNewlineMax (x:xs)
-    | x == '\n' && length xs > 2 = "\n\n" 
-    | otherwise = x:xs
+enforceTwoSequentialNewlineMax :: String -> String       
+enforceTwoSequentialNewlineMax ('\n':'\n':_) = "\n\n"
+enforceTwoSequentialNewlineMax x = x
 
 trimWhitespaceOnlyLines :: String -> String
 trimWhitespaceOnlyLines line 
@@ -38,3 +37,9 @@ trimLastWhitespaces [] = []
 trimLastWhitespaces line
     | last line == ' ' || last line == '\t' = trimLastWhitespaces $ init line
     | otherwise = line
+
+trimLastNewlines :: [String] -> [String]
+trimLastNewlines [] = []
+trimLastNewlines lines
+    | last lines == "" = trimLastNewlines $ init lines
+    | otherwise = lines 
